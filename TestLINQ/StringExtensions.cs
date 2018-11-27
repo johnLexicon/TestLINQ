a841d1c19c0fc7eef5eb33d40600014c555e5ed3
John Lundgren <john.paul.lundgren@gmail.com>
2018-11-27 05:28:21 +0100

    Added class with extension method.

---
+++ TestLINQ/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Threading;
+
+namespace TestLINQ
+{
+    public static class StringExtensions
+    {
+        public static string ConvertToTitleCase(this string source)
+        {
+            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+            TextInfo textInfo = cultureInfo.TextInfo;
+
+            return textInfo.ToTitleCase(source);
+        }
+    }
+}
